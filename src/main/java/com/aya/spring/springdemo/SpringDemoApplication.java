3e75c8d73ba5e1f78a0da2342619d4d6f1b6355e
ArtemYankovets <ArtemYankovets@gmail.com>
2017-10-10 11:58:13 +0300

    Add RequestMapping /hello

--- src/main/java/com/aya/spring/springdemo/SpringDemoApplication.java
+++ src/main/java/com/aya/spring/springdemo/SpringDemoApplication.java
@@ -2,11 +2,20 @@ package com.aya.spring.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
+
+	@RequestMapping("/hello")
+	public String showHelloWorld(){
+		return "hello-world";
+	}
+
 }
